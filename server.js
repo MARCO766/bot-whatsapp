fe762561ff8ed6b5872dbaa241e4053c8ed77860
Marco <narias76629@gmail.com>
2026-04-16 02:25:09 -0400

    fix railway port

--- server.js
+++ server.js
@@ -68,6 +68,8 @@ app.post('/webhook', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-    console.log("🚀 Servidor corriendo en puerto 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("🚀 Servidor corriendo en puerto", PORT);
 });
