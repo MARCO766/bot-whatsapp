91f58c50e041bbc95b12c95d4e9bb626a0bbfd55
Marco <narias76629@gmail.com>
2026-05-07 02:51:25 -0400

    agregue seguimiento automatico

--- server.js
+++ server.js
@@ -647,4 +647,4 @@ setTimeout(async () => {
 // 🚀 SERVIDOR
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("🚀 Servidor corriendo en puerto", PORT);
+  console.log("🚀 Servidor corriendo en puerto", PORT);});
