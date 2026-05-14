fe4494ca9cf6b07a90430b2da7b5cae193f12b38
Marco <narias76629@gmail.com>
2026-05-14 14:46:13 -0400

    agregue seguimiento automatico

--- routes/flows.js
+++ routes/flows.js
@@ -1038,6 +1038,12 @@ ${
   : ""
 }
 
+${
+  msg.tipo === "audio" && msg.imagen_url
+    ? '<audio controls style="width:260px;display:block;margin-bottom:6px;"><source src="' + msg.imagen_url + '"></audio>'
+    : ""
+}
+
       ${
         msg.contenido && !msg.contenido.startsWith("http")
         ? msg.contenido
