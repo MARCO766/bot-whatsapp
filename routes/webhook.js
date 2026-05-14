32e6f45016aab13e8e0130f287def2dd89388903
Marco <narias76629@gmail.com>
2026-05-14 12:46:09 -0400

    fix audio token

--- routes/webhook.js
+++ routes/webhook.js
@@ -135,7 +135,7 @@ if (message.audio && message.audio.id) {
       `https://graph.facebook.com/v19.0/${message.audio.id}`,
       {
         headers: {
-          Authorization: `Bearer ${conexionWebhook.token}`
+          Authorization: `Bearer ${conexion.token}`
         }
       }
     );
@@ -145,7 +145,7 @@ if (message.audio && message.audio.id) {
     const audioFile = await axios.get(audioUrlMeta, {
       responseType: "arraybuffer",
       headers: {
-        Authorization: `Bearer ${conexionWebhook.token}`
+        Authorization: `Bearer ${conexion.token}`
       }
     });
 
